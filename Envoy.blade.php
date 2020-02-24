a4b00fc1aa9c5aa87002523789050f31c26932bb
Lorenzo Milesi <lmilesi@netemedia.fr>
2020-02-24 22:47:26 +0100

    lock

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -4,7 +4,7 @@
   cd /var/www/westeros/project/
   php7.4 artisan down
   git pull origin master
-  composer install
+  php7.4 $(which composer) install
   php7.4 artisan migrate
   php7.4 artisan cache:clear
   php7.4 artisan up
