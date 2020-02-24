e6749a1d5e98b742560c85aef008a8e3651de763
Lorenzo Milesi <lmilesi@netemedia.fr>
2020-02-24 22:10:56 +0100

    composer relatif

--- project/resources/views/tasks/index.blade.php
+++ project/resources/views/tasks/index.blade.php
@@ -14,6 +14,9 @@
 
 @section('content')
   <section class="flex -mx-4 items-start">
+    <div class="w-1/4 bg-white rounded shadow mx-4">
+      @livewire('filters.tasks')
+    </div>
     @livewire('tasks.table')
     @livewire('modals.tasks.edit')
     @livewire('modals.tasks.delete')
