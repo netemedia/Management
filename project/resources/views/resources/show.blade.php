0f1c0300c3c02d25414eede1e19829caa1eb888f
Lorenzo Milesi <lmilesi@netemedia.fr>
2020-02-28 16:54:26 +0100

    ok

--- project/resources/views/resources/show.blade.php
+++ project/resources/views/resources/show.blade.php
@@ -6,6 +6,9 @@
 
 @section('content')
   <section class="flex -mx-4 items-start">
+    <div class="w-1/4 bg-white rounded shadow mx-4">
+      @livewire('filters.resources.tasks', $resource->id)
+    </div>
     @livewire('resources.tasks', $resource->id)
     @livewire('modals.tasks.edit')
     @livewire('modals.tasks.delete')
